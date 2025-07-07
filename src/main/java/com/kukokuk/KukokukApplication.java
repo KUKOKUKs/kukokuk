4b1c5ea2d47dba362b3672faeb8109245ea9c464
ksm001027 <102711270+ksm001027@users.noreply.github.com>
2025-07-07 17:22:52 +0900

    fisrt commit

---
+++ src/main/java/com/kukokuk/KukokukApplication.java
@@ -0,0 +1,13 @@
+package com.kukokuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukokukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KukokukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kukokuk/KukokukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kukokuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KukokukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
