60ebf28dd20da3a89d77a51e218009b7c1f023f9
김다영 <dayeong0120.com>
2025-07-14 11:42:39 +0900

    2025-07-14 11:41 dto패키지도 mybatis 별칭 사용하는 패키지로 등록, 마이바티스관련 수정

--- src/main/java/com/kukokuk/KukokukApplication.java
+++ src/main/java/com/kukokuk/KukokukApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.kukokuk.mapper")
+@MapperScan("com.kukokuk.repository")
 public class KukokukApplication {
 
 	public static void main(String[] args) {
