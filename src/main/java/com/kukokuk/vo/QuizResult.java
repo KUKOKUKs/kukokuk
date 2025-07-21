df3dc915ec2a1265cdd336e3f8975fb896128dd3
ksm001027 <102711270+ksm001027@users.noreply.github.com>
2025-07-21 12:28:08 +0900

    2025-07-21 12:27 add: 퀴즈 결과 vo 및 퀴즈 풀이 응답 dto 추가

--- src/main/java/com/kukokuk/vo/QuizResult.java
+++ src/main/java/com/kukokuk/vo/QuizResult.java
@@ -1,23 +1,19 @@
 package com.kukokuk.vo;
 
+import java.util.Date;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.apache.ibatis.type.Alias;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @Alias("QuizResult")
 public class QuizResult {
-
-    private int resultNo;
-    private int sessionNo;
-    private int selectedChoice;
-    private String isSuccess;      // ENUM("Y","N")
-    private String isBookmarked;   // ENUM("Y","N")
-    private String createdDate;
-    private String updatedDate;
-    private int userNo;
-    private int quizNo;
+    private int resultNo;        // 결과 고유 번호 (PK)
+    private int quizNo;          // 퀴즈 번호 (FK)
+    private int userNo;          // 사용자 번호 (FK)
+    private int selectedOption;  // 사용자가 선택한 보기 번호
+    private boolean isCorrect;   // 정답 여부
+    private Date createdDate; // 생성일
+    private Date updatedDate; // 수정일
 }
