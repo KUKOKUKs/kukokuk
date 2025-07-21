fdb6ab1ce96afce19fc6f6e7751f2d1185ee1563
shmjeongho <shimjh0628@gmail.com>
2025-07-21 09:46:40 +0900

    250721 09:46 add 받아쓰기 vo mapper service 추가

---
+++ src/main/java/com/kukokuk/vo/DictationSession.java
@@ -0,0 +1,23 @@
+package com.kukokuk.vo;
+
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.ibatis.type.Alias;
+
+@Getter
+@Setter
+@Alias("DictationSession")
+public class DictationSession {
+
+  private int dictationSessionNo; // 문제 세트 번호
+  private int userNo;             // 회원 번호
+  private Date startDate;         // 시작 시각
+  private Date endDate;           // 종료 시각
+  private int correctScore;       // 정답 점수
+  private int correctCount;       // 맞은 개수
+  private int hintUsedCount;      // 사용한 힌트 수
+  private Date createDate;        // 생성일 (NOW())
+
+
+}
