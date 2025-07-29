9601f9b3781b6ed3811380587b6625785453d18d
leejungho <lljungho@naver.com>
2025-07-29 12:25:49 +0900

    250729 12:25 mod 사용자 프로필 내용 표현식 수정중

--- src/main/java/com/kukokuk/vo/User.java
+++ src/main/java/com/kukokuk/vo/User.java
@@ -23,7 +23,7 @@ public class User {
     private String profileFilename;
     private String authProvider;
     private int level;
-    private String experiencePoints;
+    private int experiencePoints;
     private int studyDifficulty;
     private String currentSchool; // ENUM("초","중")
     private int currentGrade;
@@ -31,7 +31,7 @@ public class User {
     private String isDeleted; // ENUM("N", "Y")
     private Date createdDate;
     private Date updatedDate;
-
-    private List<String> roleNames;
+    private int maxExp; // 레벨업에 필요한 누적 경험치
+    private List<String> roleNames; // 사용자 권한 정보 목록
 
 }
