d9871d507fcfaf4798e20d8347bf1ef8ac0854b0
dayeong0120 <dayeong0120@naver.com>
2025-07-30 20:23:40 +0900

    2025-07-30 20:23 add REST API 에러응답 핸들러 추가

--- src/main/java/com/kukokuk/rest/RestControllerExceptionHandler.java
+++ src/main/java/com/kukokuk/rest/RestControllerExceptionHandler.java
@@ -2,13 +2,15 @@ package com.kukokuk.rest;
 
 import com.kukokuk.exception.AppException;
 import com.kukokuk.response.ApiResponse;
+import com.kukokuk.response.ResponseEntityUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackages = "com.kukokuk.rest")
 public class RestControllerExceptionHandler {
   @ExceptionHandler(AppException.class)
-  public ApiResponse<Void> handlerAppException(AppException ex) {
-    return ApiResponse.fail(500, ex.getMessage());
+  public ResponseEntity<ApiResponse<Void>> handlerAppException(AppException ex) {
+    return ResponseEntityUtils.fail(500, ex.getMessage());
   }
 }
