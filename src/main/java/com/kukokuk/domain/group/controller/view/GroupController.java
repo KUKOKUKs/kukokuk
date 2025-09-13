cdb5a704ea0f71ccfac1512366f0cfb12146d06e
leejungho <lljungho@naver.com>
2025-09-13 22:28:03 +0900

    250913 22:27 add 그룹 페이지 기초 세팅

--- src/main/java/com/kukokuk/domain/group/controller/view/GroupController.java
+++ src/main/java/com/kukokuk/domain/group/controller/view/GroupController.java
@@ -1,8 +1,10 @@
 package com.kukokuk.domain.group.controller.view;
 
+import com.kukokuk.domain.group.service.GroupService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,10 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/group")
 public class GroupController {
 
+    private final GroupService groupService;
+
     // 그룹 메인 페이지
     @GetMapping
-    public String groupPage() {
+    public String groupPage(Model model) {
         log.info("GroupController groupPage() 컨트롤러 실행");
+        model.addAttribute("randomGroups", groupService.getRandomGroups(10));
         return "group/main";
     }
 
--- src/main/java/com/kukokuk/domain/group/vo/Group.java
+++ src/main/java/com/kukokuk/domain/group/vo/Group.java
@@ -17,13 +17,21 @@ import org.apache.ibatis.type.Alias;
 @Alias("Group")
 public class Group {
 
-    private int groupNo;            // 그룹번호
-    private String title;           // 그룹명
-    private String password;        // 그룹 비밀번호(숫자로만 4자리) NULL 일 경우 공개 그룹
-    private String isDeleted;       // ENUM("N", "Y")
+    private int groupNo;                // 그룹번호
+    private String title;               // 그룹명
+    private String motto;               // 급훈
+    private String password;            // 그룹 비밀번호(숫자로만 4자리) NULL 일 경우 공개 그룹
+    private String isDeleted;           // ENUM("N", "Y")
     private Date createdDate;
     private Date updatedDate;
 
-    private User teacher;           // 그룹 생성자(TEACHER 권한) 정보
+    private User teacher;               // 그룹 생성자(TEACHER 권한) 정보
+
+    private int memberCount;            // 그룹 인원 수
+
+    // 천 단위 콤마 붙인 getter
+    public String getMemberCountFormatted() {
+        return String.format("%,d", memberCount);
+    }
 
 }
