ccc5a4aa6f44ee82cb53007efba74f9a17654b99
leejungho <lljungho@naver.com>
2025-09-15 20:55:16 +0900

    250915 20:53 add 페이징관련 enum 객체, 페이징 fragment, 그룹 검색 기능 추가/추후 디테일 작업 예정

--- src/main/java/com/kukokuk/domain/group/controller/api/ApiGroupController.java
+++ src/main/java/com/kukokuk/domain/group/controller/api/ApiGroupController.java
@@ -1,8 +1,18 @@
 package com.kukokuk.domain.group.controller.api;
 
+import com.kukokuk.common.dto.ApiResponse;
+import com.kukokuk.common.dto.Page;
+import com.kukokuk.common.util.ResponseEntityUtils;
+import com.kukokuk.domain.group.service.GroupService;
+import com.kukokuk.domain.group.vo.Group;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
@@ -11,6 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ApiGroupController {
 
+    private final GroupService groupService;
 
+    // 그룹 검색(페이지네이션)
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<Group>>> searchGroups(
+        @RequestParam(defaultValue = "1") int page
+        , @RequestParam String keyword) {
+        log.info("ApiGroupController searchGroups() 컨트롤러 실행");
+
+        Map<String, Object> condition = new HashMap<>();
+        condition.put("keyword", keyword);
+
+        return ResponseEntityUtils.ok(
+            groupService.getGroups(page, condition)
+        );
+    }
 
 }
