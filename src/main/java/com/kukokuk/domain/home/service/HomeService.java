4c5f8e3a6ca47de51ba4f3bee7534a79a692255a
leejungho <lljungho@naver.com>
2025-09-19 07:36:39 +0900

    fix 임시 주석처리 추후 디테일 작업 예정

--- src/main/java/com/kukokuk/domain/home/service/HomeService.java
+++ src/main/java/com/kukokuk/domain/home/service/HomeService.java
@@ -1,15 +1,11 @@
 package com.kukokuk.domain.home.service;
 
-import com.kukokuk.common.exception.AppException;
 import com.kukokuk.common.store.RedisJobStatusStore;
 import com.kukokuk.domain.study.dto.DailyStudySummaryResponse;
-import com.kukokuk.domain.study.dto.UserStudyRecommendationDto;
 import com.kukokuk.domain.study.service.StudyService;
-import com.kukokuk.domain.user.vo.User;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Log4j2
@@ -37,50 +33,50 @@ public class HomeService {
      * @param user 사용자 정보
      * @param rows 요청할 학습 자료 개수
      */
-    @Async("aiTaskExecutor")
-    public void getHomeUserDailyStudies(String jobId, User user, int rows) {
-        log.info("HomeService getHomeUserDailyStudies() 서비스 실헹");
-
-        try {
-            // 학습 이력 확인하여 가져오기 가져올 학습 자료가 없을 경우 생성
-            studyJobStatusStore.update(jobId, status -> {
-                status.setProgress(40);
-                status.setMessage("맞춤 학습 자료 생성 중...");
-            });
-
-            // 사용자의 수준과 진도에 맞는 추천 학습자료(DailyStudy) 목록을 조회하는 메소드 호출
-            List<UserStudyRecommendationDto> dtos = studyService.getUserDailyStudies(
-                user, rows);
-
-            // 매핑 중
-            studyJobStatusStore.update(jobId, status -> {
-                status.setProgress(80);
-                status.setMessage("맞춤 학습 자료 정리 중...");
-            });
-
-            // UserStudyRecommendationDto에서 응답에 필요한 정보만 반환하도록 ResponseDTO에 매핑
-            List<DailyStudySummaryResponse> result =
-                studyService.mapToDailyStudySummaryResponse(dtos);
-
-            // 완료
-            studyJobStatusStore.update(jobId, status -> {
-                status.setResult(result);
-                status.setProgress(100);
-                status.setStatus("DONE");
-                status.setMessage("맞춤 학습 자료가 완성되었습니다.");
-            });
-        } catch (Exception e) {
-            studyJobStatusStore.update(jobId, status -> {
-                status.setProgress(100);
-                status.setStatus("FAILED");
-                status.setMessage("맞춤 학습 자료 생성에 실패하였습니다.\n다시 시도해 주세요.: " + e.getMessage());
-            });
-
-            log.error("맞춤 학습자료 생성 실패: {}", e.getMessage(), e);
-            
-            // @Async 비동기 처리로 최초 요청시에 클라이언트에서 예외상황을 알 수 없음
-            throw new AppException("맞춤 학습자료 생성에 실패하였습니다.: " + e.getMessage());
-        }
-    }
+//    @Async("aiTaskExecutor")
+//    public void getHomeUserDailyStudies(String jobId, User user, int rows) {
+//        log.info("HomeService getHomeUserDailyStudies() 서비스 실헹");
+//
+//        try {
+//            // 학습 이력 확인하여 가져오기 가져올 학습 자료가 없을 경우 생성
+//            studyJobStatusStore.update(jobId, status -> {
+//                status.setProgress(40);
+//                status.setMessage("맞춤 학습 자료 생성 중...");
+//            });
+//
+//            // 사용자의 수준과 진도에 맞는 추천 학습자료(DailyStudy) 목록을 조회하는 메소드 호출
+//            List<UserStudyRecommendationDto> dtos = studyService.getUserDailyStudies(
+//                user, rows);
+//
+//            // 매핑 중
+//            studyJobStatusStore.update(jobId, status -> {
+//                status.setProgress(80);
+//                status.setMessage("맞춤 학습 자료 정리 중...");
+//            });
+//
+//            // UserStudyRecommendationDto에서 응답에 필요한 정보만 반환하도록 ResponseDTO에 매핑
+//            List<DailyStudySummaryResponse> result =
+//                studyService.mapToDailyStudySummaryResponse(dtos);
+//
+//            // 완료
+//            studyJobStatusStore.update(jobId, status -> {
+//                status.setResult(result);
+//                status.setProgress(100);
+//                status.setStatus("DONE");
+//                status.setMessage("맞춤 학습 자료가 완성되었습니다.");
+//            });
+//        } catch (Exception e) {
+//            studyJobStatusStore.update(jobId, status -> {
+//                status.setProgress(100);
+//                status.setStatus("FAILED");
+//                status.setMessage("맞춤 학습 자료 생성에 실패하였습니다.\n다시 시도해 주세요.: " + e.getMessage());
+//            });
+//
+//            log.error("맞춤 학습자료 생성 실패: {}", e.getMessage(), e);
+//            
+//            // @Async 비동기 처리로 최초 요청시에 클라이언트에서 예외상황을 알 수 없음
+//            throw new AppException("맞춤 학습자료 생성에 실패하였습니다.: " + e.getMessage());
+//        }
+//    }
 
 }
