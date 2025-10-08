feebc16fb72b643d1a70e5087671aebc6445ca3d
leejungho <lljungho@naver.com>
2025-10-08 19:27:48 +0900

    간단수정

--- src/main/java/com/kukokuk/domain/exp/vo/ExpLog.java
+++ src/main/java/com/kukokuk/domain/exp/vo/ExpLog.java
@@ -1,6 +1,5 @@
 package com.kukokuk.domain.exp.vo;
 
-import com.kukokuk.common.constant.ContentTypeEnum;
 import java.util.Date;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +14,7 @@ public class ExpLog {
 
     private int expLogNo;                   // 경험치 로그 번호
     private int userNo;                     // 경험치 획득한 사용자
-    private ContentTypeEnum contentType;    // 경험치 획득한 컨텐츠
+    private String contentType;             // 경험치 획득한 컨텐츠
     private int contentNo;                  // 경험치 획득한 컨텐츠 테이블의 식별자값
     private int expGained;                  // 획득한 경험치량
     private Date createdDate;               // 생성일자
