752c13fb523be2386e4ad8438f2ab88f110d31c9
ksm001027 <102711270+ksm001027@users.noreply.github.com>
2025-10-11 02:30:57 +0900

    2025-10-11 2:30 add: levelRankDto빌더 어노테이션 추가

--- src/main/java/com/kukokuk/domain/rank/dto/LevelRankDto.java
+++ src/main/java/com/kukokuk/domain/rank/dto/LevelRankDto.java
@@ -2,22 +2,24 @@ package com.kukokuk.domain.rank.dto;
 
 import com.kukokuk.domain.rank.vo.Rank;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
- * 레벨 랭킹 조회용 DTO
+ * 레벨 랭킹 응답용 DTO
  * 날짜 상관없이 전체 기간 레벨 기준 랭킹
  */
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class LevelRankDto {
 
     private int userNo;           // 요청한 사용자 번호
-    private List<Rank> ranks;     // 레벨 랭크 목록 (maxExp 포함)
-
-
+    private List<Rank> ranks;     // 레벨 랭크 목록
 
 }
