b06b7ab8829ce9a3ee438be90e24a99f4dddd97a
jun-geun-choi <jklucky73@gmail.com>
2025-10-15 23:21:03 +0900

    251015 - add - 스무고개 만들기 기능 및 유효성 검사

--- src/main/java/com/kukokuk/domain/twenty/controller/view/TwentyController.java
+++ src/main/java/com/kukokuk/domain/twenty/controller/view/TwentyController.java
@@ -1,11 +1,13 @@
 package com.kukokuk.domain.twenty.controller.view;
 
+import com.kukokuk.domain.twenty.dto.CreateRoom;
 import com.kukokuk.domain.twenty.dto.RoomUser;
 import com.kukokuk.domain.twenty.dto.WsUser;
 import com.kukokuk.domain.twenty.service.TwentyService;
 import com.kukokuk.domain.twenty.vo.TwentyRoom;
 import com.kukokuk.domain.user.vo.User;
 import com.kukokuk.security.SecurityUser;
+import jakarta.validation.Valid;
 import java.security.Principal;
 import java.util.HashMap;
 import java.util.List;
@@ -23,9 +25,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log4j2
 @Controller
@@ -55,11 +62,10 @@ public class TwentyController {
      */
     @GetMapping("/gameRoom/{roomNo}")
     public String gameRoom(@PathVariable int roomNo,
-        Model model,
-        @AuthenticationPrincipal SecurityUser securityUser) {
+                           Model model,
+                           @AuthenticationPrincipal SecurityUser securityUser) {
 
         // 1. 사용자 정보 -> 토큰화 -> Redis에 담기
-
         //토큰 만들기
         String token = UUID.randomUUID().toString();
         String key = "ws:token:" +  token;
@@ -80,7 +86,8 @@ public class TwentyController {
             model.addAttribute("error", "존재하지 않는 게임방입니다.");
             return "group/main";
         }
-        model.addAttribute("roomNo", 1);
+        model.addAttribute("correctAnswer", room.getCorrectAnswer());
+        model.addAttribute("roomNo", roomNo);
         List<RoomUser> list = twentyService.getTwentyPlayerList(roomNo);
         model.addAttribute("list", list);
 
@@ -91,56 +98,30 @@ public class TwentyController {
 
     }
 
-    //교사의 경우, 설정을 마치고 바로 게임방으로 이동!, PostMapping을 해야된다.
+    /**
+     * 교사의 경우: 게임방 페이지를 만들고, 게임방 페이지로 이동
+     * 1. 폼의 값을 map에 담아 서비스 객체에 담아 -> DB에 저장 -> 그 게임방을 조회
+     * 2. 그 게임방의 no를 가지고 리다이렉션으로 이동.
+     * @return
+     */
+    @PostMapping("/gameRoom")
+    public String createTwentyRoom(@Valid @ModelAttribute CreateRoom room,
+                                   BindingResult bindingResult,
+                                   RedirectAttributes attributes){
+        //폼 제출 시 유효성 검사
+        if(bindingResult.hasErrors()){
+            if(bindingResult.hasFieldErrors("title")){
+                attributes.addFlashAttribute("titleError",
+                                                         bindingResult.getFieldError("title").getDefaultMessage());
+            }
 
-//    /**
-//     * - 학생이 게임방에 입장했을 대
-//     *
-//     * @param currentRoomNo
-//     * @param principal     => 웹소켓 서버에서는 사용자 정보를 이 객체를 통해 꺼내야 한다.
-//     * @parma accessor => 웹소켓 세션에 게임방 No를 담기 위해.. => 그래야 서버 끊길 때 이 게임방No를 활용할 수 있음.
-//     * <p>
-//     * 추가로 Http에서는 @PathVariabel을 사용하지만, 웹소켓 서버에서는 @DestinationVariable을 사용
-//     */
-//    @MessageMapping("/join/{currentRoomNo}")
-//    public void joinGameRoom(@DestinationVariable int currentRoomNo, Principal principal,
-//        StompHeaderAccessor accessor) {
-//        accessor.getSessionAttributes().put("currentRoomNo", currentRoomNo);
-//
-//        // 웹소켓 세션으로 핸드쉐이킹한 로그인 사용자 정보를 다시 SecurityUser로 변경해서 로직 수행
-//        if (principal instanceof Authentication auth) {
-//            SecurityUser securityUser = (SecurityUser) auth.getPrincipal();
-//            int currentUserNo = securityUser.getUser().getUserNo();
-//            twentyService.joinGameRoom(currentUserNo, currentRoomNo);
-//        }
-//    }
-//
-//    /**
-//     * 교사가 게임 시작 버튼을 눌렀을 때!
-//     *
-//     * @param currentRoomNo
-//     * @param principal
-//     */
-//    @MessageMapping("/gameStart/{currentRoomNo}")
-//    public void gameStart(@DestinationVariable int currentRoomNo, Principal principal) {
-//        twentyService.gameStart(currentRoomNo);
-//    }
-//
-//    /**
-//     * 손들기 버튼을 눌렀을 때,
-//     *
-//     * @param currentRoomNo
-//     * @param principal
-//     */
-//    @MessageMapping("/raisehand/{currentRoomNo}")
-//    public void raiseHand(@DestinationVariable int currentRoomNo, Principal principal) {
-//        if (principal instanceof Authentication auth) {
-//            SecurityUser securityUser = (SecurityUser) auth.getPrincipal();
-//            int userNo = securityUser.getUser().getUserNo();
-//            String userNicName = securityUser.getUser().getNickname();
-//            twentyService.raiseHand(currentRoomNo, userNo, userNicName);
-//        }
-//
-//    }
-
+            if(bindingResult.hasFieldErrors("correct")){
+                attributes.addFlashAttribute("correctError",
+                                                         bindingResult.getFieldError("correct").getDefaultMessage());
+            }
+            return "redirect:/group/teacher";
+        }
+        Integer roomNo = twentyService.insertTwenthRoom(room.getGroupNo(), room.getTitle(),room.getCorrect());
+        return "redirect:/twenty/gameRoom/" + roomNo;
+    }
 }
